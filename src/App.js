fb0f198257651652f038ce5619243dc4c4138a7f
Oguzhan Kocaturk <ospirentos@gmail.com>
2020-03-04 17:01:42 +0300

    Created container elements and main structure of page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import BabylonScene from "./Components/BabylonScene";
 
 function App() {
   return (
     <div className="main-container">
-      <div className="viewport-container">World</div>
+      <div className="viewport-container">
+        <BabylonScene />
+      </div>
       <div className="modificaiton-buttons-container">Color Buttons</div>
     </div>
   );
