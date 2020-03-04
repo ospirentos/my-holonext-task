da5c365b1649f92ce72d4ab8d775552c27e97688
Oguzhan Kocaturk <ospirentos@gmail.com>
2020-03-04 14:38:52 +0300

    Initial commit, project created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main-container">
+      <div className="viewport-container">World</div>
+      <div className="modificaiton-buttons-container">Color Buttons</div>
+    </div>
+  );
+}
+
+export default App;
