9e9f864e8d67642246162d15823fba42ce0a0e2d
Oguzhan Kocaturk <ospirentos@gmail.com>
2020-03-06 12:14:21 +0300

    Updates

---
+++ precache-manifest.cea0ef53c46d1d4f942003ea8ec52211.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0f0c2b72689e77feb5ae1ebf314e3f",
+    "url": "/my-holonext-task/index.html"
+  },
+  {
+    "revision": "be55abd9abed877f1ed2",
+    "url": "/my-holonext-task/static/css/main.5f2e8fc4.chunk.css"
+  },
+  {
+    "revision": "01171969c19124212f77",
+    "url": "/my-holonext-task/static/js/2.5b506a04.chunk.js"
+  },
+  {
+    "revision": "1bc405a2f9ca5b8136d11cf29f9b2788",
+    "url": "/my-holonext-task/static/js/2.5b506a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be55abd9abed877f1ed2",
+    "url": "/my-holonext-task/static/js/main.c60504e0.chunk.js"
+  },
+  {
+    "revision": "523cb72fa5e774281770",
+    "url": "/my-holonext-task/static/js/runtime-main.dddc7435.js"
+  }
+]);
